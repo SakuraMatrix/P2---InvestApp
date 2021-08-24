94b6718816f8be376daecb0ed4425e662ad681b8
SoDaRa <46580818+SoDaRa@users.noreply.github.com>
2021-08-24 12:18:05 -0400

    Started Client Layer Branch

---
+++ ClientLayer/ClientLayer/src/main/java/com/github/InvestApp/ClientLayer/ClientLayerApplication.java
@@ -0,0 +1,13 @@
+package com.github.InvestApp.ClientLayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientLayerApplication.class, args);
+	}
+
+}
