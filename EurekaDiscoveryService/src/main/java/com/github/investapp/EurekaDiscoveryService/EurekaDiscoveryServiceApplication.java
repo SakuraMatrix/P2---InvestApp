becc41f76e94873c253ffa0bd1f6457c613d7a9e
Andrew Gregersen <andrew.gregersen@revature.net>
2021-08-30 08:36:39 -0700

    Init Discovery Service

---
+++ EurekaDiscoveryService/src/main/java/com/github/investapp/EurekaDiscoveryService/EurekaDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.investapp.EurekaDiscoveryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDiscoveryServiceApplication.class, args);
+	}
+
+}
