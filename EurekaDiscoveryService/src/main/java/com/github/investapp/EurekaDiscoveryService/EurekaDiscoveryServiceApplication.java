c558392acbad0a49215a1e464fde4a9b59d80dd2
Andrew Gregersen <andrew.gregersen@revature.net>
2021-08-30 08:37:32 -0700

    Configured basic discovery service

--- EurekaDiscoveryService/src/main/java/com/github/investapp/EurekaDiscoveryService/EurekaDiscoveryServiceApplication.java
+++ EurekaDiscoveryService/src/main/java/com/github/investapp/EurekaDiscoveryService/EurekaDiscoveryServiceApplication.java
@@ -2,12 +2,13 @@ package com.github.investapp.EurekaDiscoveryService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaDiscoveryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaDiscoveryServiceApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaDiscoveryServiceApplication.class, args);
+  }
 }
