29f68260eb04babf0c108b59051b070add1231b1
pm-1992 <paul.marquis1@gmail.com>
2021-08-31 22:43:48 -0700

    Fixing minor bug

--- AnalysisService/src/main/java/com/github/InvestApp/AnalysisService/service/AnalysisService.java
+++ AnalysisService/src/main/java/com/github/InvestApp/AnalysisService/service/AnalysisService.java
@@ -16,7 +16,7 @@ import reactor.core.publisher.Mono;
 public class AnalysisService {
 
   private final AnalysisRepository repo; 
-  static int id = 2;
+  static int keyId = 2;
 
   public AnalysisService(AnalysisRepository repo) {
     this.repo = repo;
@@ -35,8 +35,8 @@ public class AnalysisService {
     ObjectMapper objectMapper = new ObjectMapper();
     HttpHandler handler = new HttpHandler(symbol);   
     Stocks stock = objectMapper.readValue(handler.sendHttp(symbol), Stocks.class);   
-    System.out.println("The price of stock " + symbol + " is: $" + stock.getPrice());
-    stock.setId(++id);  
+    //System.out.println("The price of stock " + symbol + " is: $" + stock.getPrice());
+    stock.setId(++keyId);  
     stock.setAccount_Id(account_id); 
     stock.setOwned(owned); 
     return this.repo.addStock(stock.getId(), stock.getAccount_Id(), stock.getOwned(), stock.getSymbol(), stock.getName(), 
