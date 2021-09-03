810295b63250aa871a1666f01d5629a8bd7b48b0
SoDaRa <46580818+SoDaRa@users.noreply.github.com>
2021-09-02 22:21:04 -0400

    Fixed LoanServices

--- LoanServices/src/main/java/com/GitHub/InvestApp/LoanServices/App.java
+++ LoanServices/src/main/java/com/GitHub/InvestApp/LoanServices/App.java
@@ -10,7 +10,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
